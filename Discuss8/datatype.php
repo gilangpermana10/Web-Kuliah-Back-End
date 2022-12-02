0a9f2181f7b9fe181a053262027a286e054d75fc
gilang_permana <gilanghyper27@gmail.com>
2022-12-03 01:23:43 +0700

    add some files

---
+++ Discuss8/datatype.php
@@ -0,0 +1,9 @@
+<?php
+$mahasiswa = [
+        "nama" => "Gilang",
+        "nim" => "A11.2019.11684",
+        ];
+
+        echo "Halo, nama saya $mahasiswa[nama] nim saya adalah $mahasiswa[nim]";
+        
+?>
---
+++ Discuss8/variable.php
@@ -0,0 +1,13 @@
+<?php
+    $nim = "A11.2019.11684";
+    $nama = "Gilang";
+
+    echo "Halo, nama saya $nama nim saya adalah $nim";
+
+    $a = 10;
+    $b = 20;
+    $c = $a + $b;
+
+    echo "<br>Hasil penjumlahan $a + $b = $c <br>";
+    var_dump($c);
+?>
